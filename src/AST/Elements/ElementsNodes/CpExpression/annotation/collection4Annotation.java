23728aab40c23a1fd013d4ed56555ef3acec0959
fatima12345678 <65631981+fatima12345678@users.noreply.github.com>
2021-04-24 01:38:23 +0300

    Complete cp Expression

--- src/AST/Elements/ElementsNodes/CpExpression/annotation/collection4Annotation.java
+++ src/AST/Elements/ElementsNodes/CpExpression/annotation/collection4Annotation.java
@@ -1,7 +1,68 @@
 package AST.Elements.ElementsNodes.CpExpression.annotation;
 
+import AST.Elements.ElementsNodes.generic4Elements.array.arrName;
+import AST.Elements.ElementsNodes.generic4Elements.object.obj;
+
 public class collection4Annotation {
 
     private  functionCall4AnnotOneLine functionCall4AnnotOneLine;
-   // private arrName arrName;
+    private arrName arrName;
+    private arrayFuncRet4AnnotOneLine arrayFuncRet4AnnotOneLine;
+    private obj obj;
+    private propFuncRet4AnnotOneLine propFuncRet4AnnotOneLine;
+    private oneLine4Annotation oneLine4Annotation;
+
+    public void setFunctionCall4AnnotOneLine(functionCall4AnnotOneLine functionCall4AnnotOneLine) {
+        this.functionCall4AnnotOneLine = functionCall4AnnotOneLine;
+    }
+
+    public void setArrName(arrName arrName) {
+        this.arrName = arrName;
+    }
+
+    public void setArrayFuncRet4AnnotOneLine(arrayFuncRet4AnnotOneLine arrayFuncRet4AnnotOneLine) {
+        this.arrayFuncRet4AnnotOneLine = arrayFuncRet4AnnotOneLine;
+    }
+
+    public void setObj(obj obj) {
+        this.obj = obj;
+    }
+
+    public void setPropFuncRet4AnnotOneLine(propFuncRet4AnnotOneLine propFuncRet4AnnotOneLine) {
+        this.propFuncRet4AnnotOneLine = propFuncRet4AnnotOneLine;
+    }
+
+    public void setOneLine4Annotation(oneLine4Annotation oneLine4Annotation) {
+        this.oneLine4Annotation = oneLine4Annotation;
+    }
+
+
+
+    public functionCall4AnnotOneLine getFunctionCall4AnnotOneLine() {
+        return functionCall4AnnotOneLine;
+    }
+
+    public arrName getArrName() {
+        return arrName;
+    }
+
+    public arrayFuncRet4AnnotOneLine getArrayFuncRet4AnnotOneLine() {
+        return arrayFuncRet4AnnotOneLine;
+    }
+
+    public obj getObj() {
+        return obj;
+    }
+
+    public propFuncRet4AnnotOneLine getPropFuncRet4AnnotOneLine() {
+        return propFuncRet4AnnotOneLine;
+    }
+
+    public oneLine4Annotation getOneLine4Annotation() {
+        return oneLine4Annotation;
+    }
+
+
+
+
 }
